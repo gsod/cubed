80f4062b55af40136883b8a6a292f221fce2df29
Giuseppe Sodano <gsodano@e4r13p3.42roma.it>
2023-11-15 18:26:54 +0100

    norminettato e raycast teoricamente funzionante

--- cub3d.h
+++ cub3d.h
@@ -18,11 +18,11 @@
 # include <stdlib.h>
 # include <fcntl.h>
 # include <math.h>
-# include "../libft/libft.h"
-# include "../minilibx-linux/mlx.h"
+# include "./libft/libft.h"
+# include "./minilibx-linux/mlx.h"
 
-#define WIN_WIDTH 1280.0
-#define WIN_HEIGHT 720.0
+# define WIN_WIDTH 1280.0
+# define WIN_HEIGHT 720.0
 
 # define RAD 0.0174533
 
@@ -36,30 +36,34 @@
 # define SHIFT 65505
 # define SPACE 32
 
-int keys(int keycode, t_cub3d *cub3d);
-
 typedef struct s_player
 {
-	float ascisse_x;
-	float ordinate_y;
-	float pov;
+	float	ascisse_x;
+	float	ordinate_y;
+	float	pov;
 }	t_player;
 
 typedef struct s_cub3d
 {
-    int	w;
-	int	a;
-	int s;
-	int d;
-	int	t_right;
-	int t_left;
+	int			w;
+	int			a;
+	int			s;
+	int			d;
+	int			t_right;
+	int			t_left;
 
-	int	orientation[(int) WIN_WIDTH];
-	char **map;
-	float wall_height;
-	float radius_dim;
-	
-	t_player player;	
-}            t_cub3d;
+	int			orientation[(int) WIN_WIDTH];
+	char		**map;
+	float		wall_height;
+	float		radius_dim[(int) WIN_WIDTH];
+
+	t_player	player;
+
+}	t_cub3d;
+
+int		keys(int keycode, t_cub3d *cub3d);
+void	ft_rad(t_cub3d *cub3d);
+void	ft_pgtowall_dist(t_cub3d *cub3d, float angle, int i);
+void	ft_orientation(t_cub3d *cub3d, float angle, float dist, int i);
 
 #endif
--- raycast/ray_casting.c
+++ raycast/ray_casting.c
@@ -11,23 +11,24 @@
 /* ************************************************************************** */
 
 #include "../cub3d.h"
-
-void ft_rad(t_cub3d *cub3d) // la vista del tuo giocatore
+// la vista del tuo giocatore
+void	ft_rad(t_cub3d *cub3d)
 {
 	int	i;
 	int	angle;
 
 	angle = cub3d->player.pov - 30.0;
 	i = 0;
-	while(i < WIN_WIDTH)
+	while (i < WIN_WIDTH)
 	{
 		ft_pgtowall_dist(cub3d, angle, i);
 		angle += 60.0 / WIN_WIDTH;
 		i++;
-	} 
+	}
 }
 
-void ft_pgtowall_dist(t_cub3d *cub3d, float angle, int i) // distanza tra il giocatore e il muro
+// distanza tra il giocatore e il muro
+void	ft_pgtowall_dist(t_cub3d *cub3d, float angle, int i)
 {
 	float	sin_v;
 	float	cos_v;
@@ -36,18 +37,23 @@ void ft_pgtowall_dist(t_cub3d *cub3d, float angle, int i) // distanza tra il gio
 	sin_v = sin(angle);
 	cos_v = cos(angle);
 	dist = 0.01;
-
-	while(1)
+	while (1)
 	{
-		if (cub3d->map[(int) (cub3d->ascisse_x + dist * cos_v)][(int) (cub3d->ascisse_y + dist * sin_v)] == '1')
-			break;
+		if (cub3d->map[(int)(cub3d->player.ascisse_x + dist * cos_v)] \
+		[(int)(cub3d->player.ordinate_y + dist * sin_v)] == '1')
+			break ;
 		dist += 0.01;
 	}
-	cub3d->radius_dim[i] = dist * cos(angle - (cub3d->player.pov * RAD)); //RAD dichiarato come la conversione di un grado in radianti(perchè le funzioni sin e cos vogliono i gradi in radianti)
-	ft_orientation(cub3d, angle, dist, i);
-} //La distanza viene incrementata gradualmente fino a quando il punto sulla mappa raggiunto dalla proiezione del raggio colpisce un muro 
+	cub3d->radius_dim[i] = dist * cos (angle - (cub3d->player.pov * RAD));
+	ft_orientation (cub3d, angle, dist, i);
+}
+//RAD dichiarato come la conversione di un grado in radianti
+//(perchè le funzioni sin e cos vogliono i gradi in radianti)
+//La distanza viene incrementata gradualmente fino a quando il 
+//punto sulla mappa raggiunto dalla proiezione del raggio colpisce un muro
+// orientamento del muro colpito dal raggio (nord,sud, est o ovest)
 
-void ft_orientation(t_cub3d *cub3d, float angle, float dist, int i) // orientamento del muro colpito dal raggio (nord,sud, est o ovest)
+void	ft_orientation(t_cub3d *cub3d, float angle, float dist, int i)
 {
 	float	cos_value;
 	float	sin_value;
@@ -56,8 +62,8 @@ void ft_orientation(t_cub3d *cub3d, float angle, float dist, int i) // orientame
 
 	cos_value = cos(angle);
 	sin_value = sin(angle);
-	ascisse_x = cub3d->ascisse_x + dist * cos_value + 0.01;
-	ordinate_y = cub3d->ordinate_y + dist * sin_value;
+	ascisse_x = cub3d->player.ascisse_x + dist * cos_value + 0.01;
+	ordinate_y = cub3d->player.ordinate_y + dist * sin_value;
 	if (cub3d->map[(int)(ordinate_y - 0.01)][(int)ascisse_x] == '1')
 		cub3d->orientation[i] = 0;
 	else if (cub3d->map[(int)(ordinate_y + 0.01)][(int)ascisse_x] == '1')
@@ -66,4 +72,7 @@ void ft_orientation(t_cub3d *cub3d, float angle, float dist, int i) // orientame
 		cub3d->orientation[i] = 2;
 	else
 		cub3d->orientation[i] = 3;
-} //L'orientamento viene assegnato a cub3d->orientation[i]. (quindi se il muro è a nord, sud, est o ovest)
+}
+
+//L'orientamento viene assegnato a cub3d->orientation[i].
+// (quindi se il muro è a nord, sud, est o ovest)
