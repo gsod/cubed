348c81dc1f5e7caf6464a95f215274a380481f42
Alessio Buonomo <alessio@Alessio.PC>
2023-11-16 19:56:36 +0100

    update

--- map_build/map_check.c
+++ map_build/map_check.c
@@ -6,7 +6,7 @@
 /*   By: abuonomo <abuonomo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 14:52:26 by abuonomo          #+#    #+#             */
-/*   Updated: 2023/11/16 19:27:35 by abuonomo         ###   ########.fr       */
+/*   Updated: 2023/11/16 19:56:12 by abuonomo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,12 +27,46 @@ void	ft_exit(char *str)
 	perror(str);
 	exit(1);
 }
+
+int is_parameter(char *str)
+{
+	if((str[0] == 'N' && str[1] == 'O')
+	|| (str[0] == 'S' && str[1] == 'O')
+	|| (str[0] == 'W' && str[1] == 'E')
+	|| (str[0] == 'E' && str[1] == 'A')
+	|| (str[0] == 'S' && str[1] == ' ')
+	|| (str[0] == 'F' && str[1] == ' '))
+	{
+		return (1);
+	}
+	return (0);
+}
+
+int param_not_present(char *tmp, t_cub3d *cub3d)
+{
+	if(tmp[0] == 'N' && tmp[1] == 'O' && cub3d->no == NULL)
+		return (1);
+	if(tmp[0] == 'S' && tmp[1] == 'O' && cub3d->so == NULL)
+		return (1);
+	if(tmp[0] == 'W' && tmp[1] == 'E' && cub3d->we == NULL)
+		return (1);
+	if(tmp[0] == 'E' && tmp[1] == 'A' && cub3d->ea == NULL)
+		return (1);
+	if(tmp[0] == 'S' && tmp[1] == ' ' && cub3d->s == NULL)
+		return (1);
+	if(tmp[0] == 'F' && tmp[1] == ' ' && cub3d->f == NULL)
+		return (1);
+	if(tmp[0] == 'C' && tmp[1] == ' ' && cub3d->c == NULL)
+		return (1);
+	return (0);
+}
+
 void check_parameter(int argc, char **argv, t_cub3d *cub3d)
 {
 	char	*tmp;
 	int		fd;
 
-	if(argc > 2 || argc < 2)
+	if(argc > 2 || argc < 2 || argv[1] == NULL || argv[1][0] == '\0')
 		ft_exit("Wrong arguments");
 	if(check_cub(argv[1]))
 		ft_exit("Wrong file extension");
@@ -43,10 +77,15 @@ void check_parameter(int argc, char **argv, t_cub3d *cub3d)
 	while(tmp != NULL)
 	{
 		tmp	= get_next_line(fd);
-		if(is_parameter(tmp) && parameter_full(cub3d) < 4)
-			add_parameter(tmp, cub3d);
 		if(tmp == NULL)
 			break;
+		if(is_parameter(tmp) && param_not_present(tmp, cub3d))
+			add_parameter(tmp, cub3d);
+		else
+		{
+			free(tmp);
+			ft_exit("Game Parameters are not correct");
+		}
 		free(tmp);
 	}
 }
